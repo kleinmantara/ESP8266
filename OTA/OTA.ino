1a373423e1a50a1dbeb4cde80cdc8217cebf5ead
kleinmantara <github@a.hart-art.de>
2018-10-29 19:05:11 +0100

    Update OTA.ino

--- OTA/OTA.ino
+++ OTA/OTA.ino
@@ -26,26 +26,21 @@ even if there is a bug in your software.
 define the folling things:
 const char* host = "ESP-YourName";
 const char* ota_password = "94km9d$3";
-
-
-
 ****************************************************************************/
 
 /*
- * Call this method in setup(), directly after setup the WIFI.
+ * Call this method in setup(), directly after setup the WIFI. Don't all your freaky after this method.
  */
 void setupOTA() {
-  info(F("Starting OTA"));
-
   ArduinoOTA.setHostname(host);
   ArduinoOTA.setPassword(ota_password);
   ArduinoOTA.begin();
 
-
   int waitTimeForOTA = 2000; // 2s
 
   if (isErrorReason()) {
-    sendMessage(String(host) + " EMERGENCY OTA: " + WiFi.localIP().toString() + ", ERROR: " + ESP.getResetInfo());
+    // here is a good point to send a message, to inform you about a error
+    // sendMessage(String(host) + " EMERGENCY OTA: " + WiFi.localIP().toString() + ", ERROR: " + ESP.getResetInfo());
 
     // 1 Minute emergency OTA
     waitTimeForOTA = 60000;
@@ -59,6 +54,9 @@ void setupOTA() {
 
 }
 
+/*
+ * Call this method in loop().
+ */
 void loopOTA() {
   ArduinoOTA.handle();
 }
